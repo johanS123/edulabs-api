3d8cda5fa39cbf161cb266ced3301a9201f1668c
Johan Rodriguez <rjohan246@gmail.com>
2024-11-13 07:49:27 -0500

    ajuste llamado del middleware

--- src/routes.php
+++ src/routes.php
@@ -22,7 +22,7 @@ return function (App $app) {
     });
 
     $app->post('/api/register', [AuthController::class ,'register']);
-    $app->post('/api/login', [AuthController::class,'login']);
+    $app->post('/api/login', 'AuthController:login');
 
     $app->group('/api', function ($group) {
         $group->get('/posts', [PostController::class, 'index']);
