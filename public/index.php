6e68480f72c5e098e7a0c2fbe5a5bcf97c99b5fa
Johan Rodriguez <rjohan246@gmail.com>
2024-11-17 01:31:07 -0500

    correccion de ruta

--- public/index.php
+++ public/index.php
@@ -1,45 +1,47 @@
 <?php
 
-use Slim\Factory\AppFactory;
-use DI\Container;
-use Dotenv\Dotenv;
-use Illuminate\Database\Capsule\Manager as Capsule;
+file_put_contents('php://stderr', "Request URI: " . $_SERVER['REQUEST_URI'] . PHP_EOL);
 
-require __DIR__ . '/../vendor/autoload.php';
+// use Slim\Factory\AppFactory;
+// use DI\Container;
+// use Dotenv\Dotenv;
+// use Illuminate\Database\Capsule\Manager as Capsule;
 
-$container = new Container();
-AppFactory::setContainer($container);
+// require __DIR__ . '/../vendor/autoload.php';
 
-$app = AppFactory::create();
-$app->addErrorMiddleware(true, true, true);
+// $container = new Container();
+// AppFactory::setContainer($container);
 
-// Cargar variables de entorno
-$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->load();
+// $app = AppFactory::create();
+// $app->addErrorMiddleware(true, true, true);
 
-// Configuración de base de datos
-$capsule = new Capsule();
-$capsule->addConnection([
-    'driver'    => 'mysql',
-    'host'      => $_ENV['DB_HOST'],
-    'database'  => $_ENV['DB_DATABASE'],
-    'username'  => $_ENV['DB_USERNAME'],
-    'password'  => $_ENV['DB_PASSWORD'],
-    'charset'   => 'utf8',
-    'collation' => 'utf8_unicode_ci',
-    'prefix'    => '',
-]);
+// // Cargar variables de entorno
+// $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+// $dotenv->load();
 
-// Configurar Eloquent para usar el acceso global
-$capsule->setAsGlobal();
-$capsule->bootEloquent();
+// // Configuración de base de datos
+// $capsule = new Capsule();
+// $capsule->addConnection([
+//     'driver'    => 'mysql',
+//     'host'      => $_ENV['DB_HOST'],
+//     'database'  => $_ENV['DB_DATABASE'],
+//     'username'  => $_ENV['DB_USERNAME'],
+//     'password'  => $_ENV['DB_PASSWORD'],
+//     'charset'   => 'utf8',
+//     'collation' => 'utf8_unicode_ci',
+//     'prefix'    => '',
+// ]);
 
-// Colocar Capsule en el contenedor para que esté disponible en el resto de la aplicación
-$container->set('db', function () use ($capsule) {
-    return $capsule;
-});
+// // Configurar Eloquent para usar el acceso global
+// $capsule->setAsGlobal();
+// $capsule->bootEloquent();
 
-// Cargar rutas
-(require __DIR__ . '/../src/routes.php')($app);
+// // Colocar Capsule en el contenedor para que esté disponible en el resto de la aplicación
+// $container->set('db', function () use ($capsule) {
+//     return $capsule;
+// });
 
-$app->run();
+// // Cargar rutas
+// (require __DIR__ . '/../src/routes.php')($app);
+
+// $app->run();
