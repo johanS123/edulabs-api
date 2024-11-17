3aa62b3cc4aacdc3760ea35b74567627ec5cfe5e
Johan Rodriguez <rjohan246@gmail.com>
2024-11-17 16:33:10 -0500

    se eliminar archivo vercel

--- public/index.php
+++ public/index.php
@@ -1,47 +1,45 @@
 <?php
 
-file_put_contents('php://stderr', "Request URI: " . $_SERVER['REQUEST_URI'] . PHP_EOL);
+use Slim\Factory\AppFactory;
+use DI\Container;
+use Dotenv\Dotenv;
+use Illuminate\Database\Capsule\Manager as Capsule;
 
-// use Slim\Factory\AppFactory;
-// use DI\Container;
-// use Dotenv\Dotenv;
-// use Illuminate\Database\Capsule\Manager as Capsule;
+require __DIR__ . '/../vendor/autoload.php';
 
-// require __DIR__ . '/../vendor/autoload.php';
+$container = new Container();
+AppFactory::setContainer($container);
 
-// $container = new Container();
-// AppFactory::setContainer($container);
+$app = AppFactory::create();
+$app->addErrorMiddleware(true, true, true);
 
-// $app = AppFactory::create();
-// $app->addErrorMiddleware(true, true, true);
+// Cargar variables de entorno
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
 
-// // Cargar variables de entorno
-// $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
-// $dotenv->load();
+// Configuración de base de datos
+$capsule = new Capsule();
+$capsule->addConnection([
+    'driver'    => 'mysql',
+    'host'      => $_ENV['DB_HOST'],
+    'database'  => $_ENV['DB_DATABASE'],
+    'username'  => $_ENV['DB_USERNAME'],
+    'password'  => $_ENV['DB_PASSWORD'],
+    'charset'   => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix'    => '',
+]);
 
-// // Configuración de base de datos
-// $capsule = new Capsule();
-// $capsule->addConnection([
-//     'driver'    => 'mysql',
-//     'host'      => $_ENV['DB_HOST'],
-//     'database'  => $_ENV['DB_DATABASE'],
-//     'username'  => $_ENV['DB_USERNAME'],
-//     'password'  => $_ENV['DB_PASSWORD'],
-//     'charset'   => 'utf8',
-//     'collation' => 'utf8_unicode_ci',
-//     'prefix'    => '',
-// ]);
+// Configurar Eloquent para usar el acceso global
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
 
-// // Configurar Eloquent para usar el acceso global
-// $capsule->setAsGlobal();
-// $capsule->bootEloquent();
+// Colocar Capsule en el contenedor para que esté disponible en el resto de la aplicación
+$container->set('db', function () use ($capsule) {
+    return $capsule;
+});
 
-// // Colocar Capsule en el contenedor para que esté disponible en el resto de la aplicación
-// $container->set('db', function () use ($capsule) {
-//     return $capsule;
-// });
+// Cargar rutas
+(require __DIR__ . '/../src/routes.php')($app);
 
-// // Cargar rutas
-// (require __DIR__ . '/../src/routes.php')($app);
-
-// $app->run();
+$app->run();
