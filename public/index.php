b2f85021e904f04cca316afd92bc5d38f187b52e
Johan Rodriguez <rjohan246@gmail.com>
2024-11-18 06:42:52 -0500

    omitir conexion

--- public/index.php
+++ public/index.php
@@ -8,17 +8,17 @@ use DI\Container;
 require __DIR__ . '/../vendor/autoload.php';
 
 // Inicializar la configuración de la base de datos
-require __DIR__ . '/../src/Config/database.php';
-$capsule = Database::initialize();
+// require __DIR__ . '/../src/Config/database.php';
+// $capsule = Database::initialize();
 
-$container = new Container();
-AppFactory::setContainer($container);
+// $container = new Container();
+// AppFactory::setContainer($container);
 
 $app = AppFactory::create();
 
-$container->set('db', function () use ($capsule) {
-    return $capsule;
-});
+// $container->set('db', function () use ($capsule) {
+//     return $capsule;
+// });
 
 // Cargar rutas
 (require __DIR__ . '/../src/routes.php')($app);
