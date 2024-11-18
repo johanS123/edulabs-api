f84bd97a86c5c4060608958ad867bfa925739a1a
Johan Rodriguez <rjohan246@gmail.com>
2024-11-18 18:56:43 -0500

    se agrega configuracion de cors

--- src/routes.php
+++ src/routes.php
@@ -13,7 +13,7 @@ return function (App $app) {
     $app->add(function ($request, $handler) {
         $response = $handler->handle($request);
         return $response
-                ->withHeader('Access-Control-Allow-Origin', '*')
+                ->withHeader('Access-Control-Allow-Origin', 'https://rainbow-lily-9716c3.netlify.app,https://www.thunderclient.com')
                 ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
                 ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     });
